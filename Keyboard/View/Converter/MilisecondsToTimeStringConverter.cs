746324d754ca12791cf221f6dc544f9ca446e691
Kamil Blazejczyk <kamileczus@gmail.com>
2025-09-04 09:02:20 +0200

    add time converter

---
+++ Keyboard/View/Converter/MilisecondsToTimeStringConverter.cs
@@ -0,0 +1,19 @@
+﻿using System.Globalization;
+using System.Windows.Data;
+
+namespace Keyboard.View.Converter;
+
+public class MilisecondsToTimeStringConverter : IValueConverter
+{
+    public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+    {
+        long ms = (long)value;
+        long mili = ms % 10;
+        long sec = ms / 10 % 60;
+        long min = ms / 600;
+        return $"{min:00}:{sec:00}.{mili:0}";
+    }
+
+    public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        => throw new NotImplementedException();
+}
