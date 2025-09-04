b8d7aadf26a1e1dbd85061ab94be0934bf8148a6
Kamil Blazejczyk <kamileczus@gmail.com>
2025-09-04 07:21:47 +0200

    initial

---
+++ Keyboard/ViewModel/BaseViewModel.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel;
+
+namespace Keyboard.ViewModel;
+
+public abstract class BaseViewModel : INotifyPropertyChanged
+{
+    public event PropertyChangedEventHandler? PropertyChanged;
+
+    protected void OnPropertyChanged(string property)
+        => PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(property));
+}
